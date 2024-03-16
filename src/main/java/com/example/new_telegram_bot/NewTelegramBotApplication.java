50a0eb0f31859e1c41c2c349c33cbef8b81c2fd0
Ilya Bogdanov <mopka9@gmail.com>
2024-03-16 03:37:14 +0300

    test_bot

---
+++ src/main/java/com/example/new_telegram_bot/NewTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.new_telegram_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewTelegramBotApplication.class, args);
+	}
+
+}
